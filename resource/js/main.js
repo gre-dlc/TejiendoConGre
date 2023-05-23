ac695c82d15dba5b3e1a4902aac866d4425071e2
gre-dlc <delacruzdamiangrecia@gmail.com>
2023-05-23 23:02:39 +0200

    gallery products

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,6 +1,7 @@
 let whatsappButton = document.querySelector('#whatsappButton');
 whatsappButton.addEventListener("click", function () {
   let phoneNumber = "+51915349452";
-  let whatsappLink = "https://wa.me/" + phoneNumber;
+  let message = "Estoy interesado en el siguiente producto";
+  let whatsappLink = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);;
   window.open(whatsappLink, "_blank");
 });
